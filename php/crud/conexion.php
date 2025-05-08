5a64a3927fc78e3d3c4da42b16523c0567cf4517
Henry676 <hdhm2001@gmail.com>
2025-05-07 18:58:55 -0600

    Realizando pruebas

--- php/crud/conexion.php
+++ php/crud/conexion.php
@@ -3,7 +3,7 @@
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = "";
-$db_name = "test_empleados";
+$db_name = "users_system";
  
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
  
